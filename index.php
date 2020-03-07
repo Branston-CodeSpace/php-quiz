7646e62daf25ae20bc4e5eb4e3e8faf19e74c140
Branston <brnstnmrng@gmail.com>
2020-03-07 11:28:07 +0200

    Added pictures

--- index.php
+++ index.php
@@ -53,24 +53,34 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <h1>Time to test your general knowledge!</h1>
+    <div>
+      <h1 class="heading">Time to test your general knowledge!</h1>
+    </div>
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </head>
 
   <body>
     <!--Looping the questions and answers-->
     <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> method="post">
-    <?php foreach($Quiz as $key => $answers):
-    $n = 0;
-    $m = $m + 1;
-    echo $key ?><br>
-    <?php foreach($answers as $a):
-    $n = $n + 1;?>
-    <input type="radio" name="answers<?php echo $m; ?>" value="ans<?php echo $n; ?>">
-    <label><?php echo $a; ?></label><br>
-    <?php endforeach;
-        endforeach; ?>
+        <?php foreach($Quiz as $key => $answers):
+        echo '<div class="answers">';
+        echo '<div class="questions">';
+        $n = 0;
+        $m = $m + 1;
+        echo $key ?>
+      </div><br>
+      <?php foreach($answers as $a):
+      $n = $n + 1;?>
+      <input type="radio" name="answers<?php echo $m; ?>" value="ans<?php echo $n; ?>">
+      <label><?php echo $a; ?></label><br>
+      <?php endforeach;?>
+    </div>
+    <?php  endforeach;?>
     <input type="submit" value="Submit">
     
     <?php
